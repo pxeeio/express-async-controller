f770457a9515fab44b42d5c9dcafc585bf43e5ca
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-08-03 00:28:17 -0700

    refactor: separate function implementation and types

--- src/Controller.ts
+++ src/Controller.ts
@@ -1,84 +1,12 @@
-import type {NextFunction, Request as ExpressRequest, Response as ExpressResponse} from 'express';
 import asyncHandler from 'express-async-handler';
 
-/**
- * Converts an async function type to a synchronous function type.
- */
-type SynchronizedFunction<TFunction> = TFunction extends (...args: infer Args) => infer Return
-    ? (...args: Args) => Awaited<Return>
-    : never;
-
-type SynchronizedFunctionDictionary<T extends object> = {
-    [key in keyof T]: SynchronizedFunction<T[key]>;
-};
-
-type BaseRequest = {
-    body?: Record<string, unknown>;
-    params?: ExpressRequest['params'];
-    query?: ExpressRequest['query'];
-};
-
-type BaseResponse = {
-    body?: Record<string, unknown>;
-    locals?: ExpressResponse['locals'];
-};
-
-/**
- * Adds any properties missing in `BaseRequest` and makes them required.
- * 
- * `body`, `params`, and `query` cannot be optional.
- *
- * If they aren't specified, its default type is attached to the Request.
- *
- * Additionally, it makes each property's object properties optional.
- */
-export type Request<TRequest extends BaseRequest = BaseRequest> = Required<{
-    [key in keyof TRequest]: Partial<TRequest[key]>;
-}> & {
-    [key in Exclude<keyof BaseRequest, keyof TRequest>]: key extends 'body'
-        ? Partial<ExpressRequest['body']>
-        : key extends 'params'
-        ? Partial<ExpressRequest['params']>
-        : Partial<ExpressRequest['query']>;
-};
-
-/**
- * Adds any properties missing in `BaseResponse` and makes them required.
- * 
- * `body` can be optional.
- * `locals` cannot be optional.
- * 
- * If they aren't specified, its default type is attached to the Response.
- *
- * Additionally, it makes each property's object properties optional.
- */
-export type Response<TResponse extends BaseResponse = BaseResponse> = Required<TResponse> & {
-    [key in Exclude<keyof BaseResponse, keyof TResponse>]: key extends 'body'
-        ? BaseResponse['body']
-        : Partial<ExpressResponse['locals']>;
-};
-
-type HTTPContext<TRequest extends Request, TResponse extends Response = Response> = {
-    request: ExpressRequest<
-        TRequest['params'],
-        TResponse['body'],
-        TRequest['body'],
-        TRequest['query'],
-        TResponse['locals']
-    >;
-    response: ExpressResponse<TResponse['body'], TResponse['locals']>;
-};
-
-export interface AsynchronousRequestHandler<
-    TRequest extends Request = Request,
-    TResponse extends Response = Response,
-> {
-    (
-        request: HTTPContext<TRequest, TResponse>['request'],
-        response: HTTPContext<TRequest, TResponse>['response'],
-        next: NextFunction,
-    ): void | Promise<void>;
-}
+import type {
+    AsynchronousRequestHandler,
+    BaseRequest,
+    Request,
+    SynchronizedFunction,
+    SynchronizedFunctionDictionary,
+} from './types';
 
 type ControllerDefinition = {
     [key: string]: BaseRequest | undefined;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import Controller from './Controller';
 
+export * from './Controller';
+export * from './types';
 export default Controller;
